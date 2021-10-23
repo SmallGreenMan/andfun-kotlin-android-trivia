a364d17ae86d735393e269dfa4d215771b3707ab
Alexandr Voishel <avoishel@gmail.com>
2021-10-23 21:04:05 +0300

    Lesson 3, step №28 Adding Animation

--- app/src/main/java/com/example/android/navigation/MainActivity.kt
+++ app/src/main/java/com/example/android/navigation/MainActivity.kt
@@ -16,26 +16,41 @@
 
 package com.example.android.navigation
 
+import android.app.ActionBar
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.example.android.navigation.databinding.ActivityMainBinding
 
+
 class MainActivity : AppCompatActivity() {
+    private lateinit var drawerLayout: DrawerLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         @Suppress("UNUSED_VARIABLE")
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        drawerLayout = binding.drawerLayout
+
 
         val navConrtoller = this.findNavController(R.id.myNavHostFragment)
-        NavigationUI.setupActionBarWithNavController(this, navConrtoller)
+        NavigationUI.setupActionBarWithNavController(this, navConrtoller, drawerLayout)
+
+        navConrtoller.addOnDestinationChangedListener { controller, destination, arguments ->  }
+
+
+        NavigationUI.setupWithNavController(binding.navView, navConrtoller)
     }
 
     override fun onSupportNavigateUp(): Boolean {
         val navController = this.findNavController(R.id.myNavHostFragment)
-        return navController.navigateUp()
+
+        //return navController.navigateUp()
+        return NavigationUI.navigateUp(navController, drawerLayout)
     }
 }
